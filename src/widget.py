bdc32dc5e7bf1c4726a4025d6c3e5ca9609263b2
Daniil <daaaniil1234@gmail.com>
2025-05-22 21:58:54 +0300

    add func get_date

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,7 @@
-from src.masks import get_mask_card_number, get_mask_account
-from typing import Union
 import datetime
+from typing import Union
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(type_and_number: Union[str]) -> Union[str]:
@@ -18,3 +19,11 @@ def mask_account_card(type_and_number: Union[str]) -> Union[str]:
         return f"{account_type} {get_mask_account(account_number)}"
     else:
         return f"{account_type} {get_mask_card_number(account_number)}"
+
+
+def get_date(user_date: Union[str]) -> Union[str]:
+    """Функция, которая принимает строку с датой и возвращает в измененном формате"""
+    date_format = datetime.datetime.strptime(user_date, "%Y-%m-%dT%H:%M:%S.%f")
+    new_date = date_format.strftime("%d.%m.%Y")
+
+    return new_date
