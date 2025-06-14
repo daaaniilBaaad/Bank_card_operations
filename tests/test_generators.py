2530b6186c81292379489400e8eacbef7c44ac62
daaaniil <daaaniil1234@gmail.com>
2025-06-14 21:13:29 +0300

    Create test_generators.py. Add tests.

---
+++ tests/test_generators.py
@@ -0,0 +1,73 @@
+import pytest
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+
+@pytest.mark.parametrize(
+    "currency, expected", [
+        ("USD", {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+    }),
+        ("RUB", {
+          "id": 939719570,
+          "state": "EXECUTED",
+          "date": "2018-06-30T02:08:58.425572",
+          "operationAmount": {
+              "amount": "9824.07",
+              "currency": {
+                  "name": "руб",
+                  "code": "RUB"
+              }
+          },
+          "description": "Перевод организации",
+          "from": "Счет 75106830613657916952",
+          "to": "Счет 11776614605963066702"
+    }),
+        ("EUR", {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {
+            "amount": "79114.93",
+            "currency": {
+                "name": "eur",
+                "code": "EUR"
+            }
+        },
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188"
+    })
+    ]
+)
+def test_filter_by_currency(currency, expected, opers):
+    _generator = filter_by_currency(opers, currency)
+    assert next(_generator) == expected
+
+
+def test_transaction_descriptions(transactions):
+    generator = transaction_descriptions(transactions)
+    assert next(generator) == "Перевод организации"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == ""
+    assert next(generator) == "Перевод с карты на карту"
+    assert next(generator) == "Перевод организации"
+
+
+def test_card_number_generator():
+    _generator = card_number_generator(1, 3)
+    assert next(_generator) == "0000 0000 0000 0001"
+    assert next(_generator) == "0000 0000 0000 0002"
+    assert next(_generator) == "0000 0000 0000 0003"
+    with pytest.raises(StopIteration):
+        next(_generator)
