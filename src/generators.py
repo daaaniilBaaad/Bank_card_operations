2c6e8b6a2fc9fe08fce13eae5aba12ba0192de39
daaaniil <daaaniil1234@gmail.com>
2025-06-14 21:40:46 +0300

    Added little changes.

--- src/generators.py
+++ src/generators.py
@@ -17,36 +17,36 @@ _opers = [
         "to": "Счет 11776614605963066702"
     },
     {
-          "id": 939719570,
-          "state": "EXECUTED",
-          "date": "2018-06-30T02:08:58.425572",
-          "operationAmount": {
-              "amount": "9824.07",
-              "currency": {
-                  "name": "руб",
-                  "code": "RUB"
-              }
-          },
-          "description": "Перевод организации",
-          "from": "Счет 75106830613657916952",
-          "to": "Счет 11776614605963066702"
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {
+                "name": "руб",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
     },
     {
-              "id": 142264268,
-              "state": "EXECUTED",
-              "date": "2019-04-04T23:20:05.206878",
-              "operationAmount": {
-                  "amount": "79114.93",
-                  "currency": {
-                      "name": "USD",
-                      "code": "USD"
-                  }
-              },
-              "description": "Перевод со счета на счет",
-              "from": "Счет 19708645243227258542",
-              "to": "Счет 75651667383060284188"
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {
+            "amount": "79114.93",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188"
     },
-   {
+    {
         "id": 142264268,
         "state": "EXECUTED",
         "date": "2019-04-04T23:20:05.206878",
@@ -61,7 +61,8 @@ _opers = [
         "from": "Счет 19708645243227258542",
         "to": "Счет 75651667383060284188"
     }
-    ]
+]
+
 
 def filter_by_currency(operations: list[dict], currency: str) -> Generator:
     """
@@ -78,13 +79,13 @@ def transaction_descriptions(transactions_list: list) -> Generator[str]:
         yield transaction["description"]
 
 
-def card_number_generator(start: int, end: int) -> Generator:
+def card_number_generator(start: int, stop: int) -> Generator:
     """Функция генератор, которая выдает номера банковских карт в формате XXXX XXXX XXXX XXXX"""
-    for i in range(start, end+1):
+    for i in range(start, stop + 1):
         card_number = []
         num = str(i).zfill(16)
         for j in range(0, 16, 4):
-            card_number.append(num[j:j+4])
+            card_number.append(num[j : j + 4])
         yield " ".join(card_number)
 
 # a = card_number_generator(1, 5)
